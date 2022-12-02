c38f378c0d356a6d6edadca7a5fe6bce7176fd2d
Yassyass181104 <119436334+Yassyass181104@users.noreply.github.com>
2022-12-02 14:59:52 +0100

    Add files via upload

---
+++ MP2-2022/src/main/java/ch/epfl/cs107/play/game/icrogue/area/ICRogueRoom.java
@@ -0,0 +1,25 @@
+package ch.epfl.cs107.play.game.icrogue.area;
+
+import ch.epfl.cs107.play.game.areagame.Area;
+import ch.epfl.cs107.play.game.areagame.actor.Background;
+import ch.epfl.cs107.play.game.areagame.actor.Foreground;
+import ch.epfl.cs107.play.math.DiscreteCoordinates;
+
+public abstract class ICRogueRoom extends Area {
+    @Override
+    public String getTitle() {
+        return "ICRogueRoom";
+    } // a changer
+    @Override
+    public DiscreteCoordinates getPlayerSpawnPosition() {
+        return new DiscreteCoordinates(2,10);
+    }
+
+    protected void createArea() {
+        // Base
+        registerActor(new Background(this));
+        registerActor(new Foreground(this));
+    }
+
+
+}
